dc4339f9704d311c5f2e1a92c2e509d094bed929
wolfe60629 <jawolfe@tds.net>
2020-06-08 13:44:37 -0400

    Adding For Linux Php Build

--- LoginProc.php
+++ LoginProc.php
@@ -2,6 +2,8 @@
 #DEFINE
 session_start();
 $config = parse_ini_file('C:\ServerFolders\PHP\Project\db.ini');
+#Temporary INI FIle For Linux Server
+$config = parse_ini_file('/home/jeremy/Documents/SoftwareEngineeringProject/Temporary/db.ini');
 $username = $_POST['username'];
 $password = $_POST['password'];
 $dbHost = $config['host'];
@@ -26,7 +28,7 @@ error_reporting(-1);
 if ($_POST["username"] || $_POST["password"])
 {
 
-    #Connect To Database 
+    #Connect To Database
 	$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 	#Check Connection
@@ -35,26 +37,26 @@ if ($_POST["username"] || $_POST["password"])
         echo "Connection could not be established.<br />";
         die(print_r(sqlsrv_errors() , true));
 	}
-	
+
 	#Query Database
 	$query = "select count(*) as \"Count\" from users where username= '" . $username . "' AND password_hash = HASHBYTES('SHA2_512', '" . $password . "');";
     $result = sqlsrv_query($conn , $query);
 
     $row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC);
-	
-	
+
+
 	#CHECK TO SEE IF CRED ARE RIGHT
-			if ($row['Count'] == 1) { 
+			if ($row['Count'] == 1) {
 				echo "1";
 				$_SESSION['userlogin'] = $username;
-			}else { 
+			}else {
 				$message = "Username and/or Password incorrect.";
 				echo $message;
-				
+
 			}
 		sqlsrv_close($conn);
-		
+
     }
-	
+
 
 ?>
